5e3ff085e93536c3bd39ece72864762fd6f4c770
Json Ding <dingmingmeng@live.com>
2015-07-14 15:34:52 +0800

    Merge branch 'master' of 192.168.0.20:android/daxue into dev_ding
    Conflicts:
    	app/src/main/AndroidManifest.xml
    	app/src/main/java/com/jingcai/apps/aizhuan/activity/index/fragment/IndexMessageFragment.java

--- app/src/main/java/com/jingcai/apps/aizhuan/activity/index/fragment/IndexMessageFragment.java
+++ app/src/main/java/com/jingcai/apps/aizhuan/activity/index/fragment/IndexMessageFragment.java
@@ -1,14 +1,17 @@
 package com.jingcai.apps.aizhuan.activity.index.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.jingcai.apps.aizhuan.R;
 import com.jingcai.apps.aizhuan.activity.base.BaseFragment;
+import com.jingcai.apps.aizhuan.activity.message.MessageCommentActivity;
 import com.jingcai.apps.aizhuan.adapter.message.MessageListAdapter;
 import com.jingcai.apps.aizhuan.entity.TestMessageBean;
 
