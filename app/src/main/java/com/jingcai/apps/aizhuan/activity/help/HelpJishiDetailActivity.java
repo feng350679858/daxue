4db9d652aa1f9deacba93b311a96eb04f871a77e
lejingw <lejingw@163.com>
2015-08-05 20:23:01 +0800

    1

--- app/src/main/java/com/jingcai/apps/aizhuan/activity/help/HelpJishiDetailActivity.java
+++ app/src/main/java/com/jingcai/apps/aizhuan/activity/help/HelpJishiDetailActivity.java
@@ -1,7 +1,5 @@
 package com.jingcai.apps.aizhuan.activity.help;
 
-import android.content.ClipData;
-import android.content.ClipboardManager;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.Message;
