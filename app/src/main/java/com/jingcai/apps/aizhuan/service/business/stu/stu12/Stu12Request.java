070bece9cf5970f3b9987766a83a7b07d0758744
Json Ding <dingmingmeng@live.com>
2015-08-10 15:45:52 +0800

    个人信用

---
+++ app/src/main/java/com/jingcai/apps/aizhuan/service/business/stu/stu12/Stu12Request.java
@@ -0,0 +1,33 @@
+package com.jingcai.apps.aizhuan.service.business.stu.stu12;
+
+import com.jingcai.apps.aizhuan.service.base.BaseRequest;
+import com.jingcai.apps.aizhuan.service.business.BizConstant;
+
+public class Stu12Request extends BaseRequest {
+    private Student student;
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    @Override
+    public String getTranscode() {
+        return BizConstant.BTZ_STU_12;
+    }
+
+    public class Student{
+        private String studentid;
+
+        public String getStudentid() {
+            return studentid;
+        }
+
+        public void setStudentid(String studentid) {
+            this.studentid = studentid;
+        }
+    }
+}
